463f9d94c657d932beb3ebdc53a742804320342a
Chuka <g00414637@atu.ie>
2023-12-11 10:31:33 +0000

    finished exam

--- src/main/java/ie/atu/Trawler.java
+++ src/main/java/ie/atu/Trawler.java
@@ -1,4 +1,41 @@
 package ie.atu;
 
 public class Trawler {
+    String name;
+    String id;
+    String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Trawler{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
 }
